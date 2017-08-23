30d7384e99b98438c9e0e4537c022f1729bf1b81
Max Scher and Nathan Stewart <max@>
2017-08-23 16:08:56 -0700

    create charity model

---
+++ src/app/charity.model.ts
@@ -0,0 +1,3 @@
+export class Charity {
+  constructor(public title: string, public description: string, public managers: string, public targetAmount: number, public ethics: string, public mission: string) {}
+}
