b77c58d583050001d5860b7f1fced58cd713edcd
Max Scher and Nathan Stewart <max@>
2017-08-23 16:18:52 -0700

    implement charity service into charities component

--- src/app/charities/charities.component.ts
+++ src/app/charities/charities.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Charity } from '../charity.model';
+import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { CharityService } from '../charity.service';
 
 @Component({
   selector: 'app-charities',
   templateUrl: './charities.component.html',
-  styleUrls: ['./charities.component.css']
+  styleUrls: ['./charities.component.css'],
+  providers: [CharityService]
 })
 export class CharitiesComponent implements OnInit {
+  charities: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  addingNewCharity = false;
+
+  constructor(private router: Router, private charityService: CharityService) { }
 
   ngOnInit() {
+    this.charities = this.charityService.getCharities();
+  }
+
+  goToDetailPage(clickedCharity) {
+    this.router.navigate(['charities', clickedCharity.$key])
   }
 
 }
