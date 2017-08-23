063657e89912df5e3e86bd44bf9622e81e828e9f
Max Scher and Nathan Stewart <max@>
2017-08-23 15:05:51 -0700

    add ideas model with attributes

---
+++ src/app/ideas.model.ts
@@ -0,0 +1,3 @@
+export class Ideas {
+  constructor(public title: string, public description: string, public managers: string, public mvp: string, public targetAmount: string, public rewards: string) {}
+}
